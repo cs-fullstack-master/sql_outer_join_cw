3989d824f843a9c6df8696778c34c7adbbe11c83
Kevin Yancy CodeCrew <40698421+Kevin-CodeCrew@users.noreply.github.com>
2019-04-30 12:20:14 -0500

    Create dbsetup.sql

---
+++ dbsetup.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS departments (
+                                           department_id serial PRIMARY KEY,
+                                           department_name VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS employees (
+                                         employee_id serial PRIMARY KEY,
+                                         employee_name VARCHAR (255),
+                                         department_id INTEGER
+);
+
+
+INSERT INTO departments (department_name)
+VALUES
+('Sales'),
+('Marketing'),
+('HR'),
+('IT'),
+('Production');
+
+INSERT INTO employees (
+    employee_name,
+    department_id
+)
+VALUES
+('Bette Nicholson', 1),
+('Christian Gable', 1),
+('Joe Swank', 2),
+('Fred Costner', 3),
+('Sandra Kilmer', 4),
+('Julia Mcqueen', NULL);
